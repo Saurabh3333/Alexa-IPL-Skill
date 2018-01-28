e3b2665728cdc06bcc585418cf276197f3d3e97d
Saurabh Shubham <saurabh.friday@gmail.com>
2018-01-28 23:45:12 +0530

    ss

--- microservices/www/src/server.js
+++ microservices/www/src/server.js
@@ -1,79 +1,14 @@
-var Alexa = require('alexa-sdk');
-var http = require('http');
-
-exports.handler = function(event, context, callback){
-  var alexa = Alexa.handler(event, context);
-  alexa.registerHandlers(handlers);
-  alexa.execute();
+const Alexa = require('alexa-sdk');
+ 
+exports.handler = function(event, context, callback) {
+    const alexa = Alexa.handler(event, context, callback);
+    alexa.appId = 'amzn1.ask.skill.77a961da-da8f-4d6a-9f4b-71f99eacb92d'; // APP_ID is your skill id which can be found in the Amazon developer console where you create the skill.
+    alexa.execute();
 };
 
-var handlers = {
-  'LaunchRequest': function () {
-    this.emit('TrumpSays');
-  },
-  'TrumpSays': function() {
-
-    trumpSaysHttp((data) => {
-      var outputSpeech= 'Here goes a Random yet Funny quote ,by the POTUS, Donald Trump! ' + data.value + ' hmmmmm ..... Do you want more?';
-
-
-      this.emit(':ask', outputSpeech,' Do you want the next quote?');
-  }
-);
-  },
-  'YesIntent': function () {
-    this.emit('TellMore');
-  },
-  'TellMore': function() {
-
-    trumpSaysHttp((data) => {
-      var outputSpeech= data.value + ' Do you want the next quote?';
-
-
-      this.emit(':ask', outputSpeech,' Do you want the next quote?');
-  }
-);
-  },
-  'AMAZON.NoIntent': function () {
-      this.emit(':tell',"OK! I know you couldnt take it more! LOL ... Bye! ");
-  },
-  'AMAZON.HelpIntent': function () {
-      this.emit(':tell',"I say a random quote said by none other than the POTUS Himself ! Haha ,you guessed it rite,its about Donald Trump! GOD BLESS AMERICA, Indeed!");
-  },
-  'AMAZON.CancelIntent': function () {
-      this.emit(':tell', "Okay!");
-  },
-  'AMAZON.StopIntent': function () {
-      this.emit(':tell', "Goodbye!");
-  }
+const handlers = {
+    'HelloWorldIntent' : function() {
+        //emit response directly
+        this.emit(':tell', 'Hello World!');
+    }
 };
-
-
-function trumpSaysHttp(callback) {
-  //http://api.open-notify.org/astros.json
-  //https://api.tronalddump.io/random/quote
-  var options = {
-    host: 'api.tronalddump.io',
-    port: 80,
-    path: '/random/quote',
-    method: 'GET'
-  };
-
-  var req = http.request(options, res => {
-      res.setEncoding('utf8');
-      var returnData = "";
-
-      res.on('data', chunk => {
-          returnData = returnData + chunk;
-      });
-
-      res.on('end', () => {
-        var result = JSON.parse(returnData);
-
-        callback(result);
-
-      });
-
-  });
-  req.end();
-}
