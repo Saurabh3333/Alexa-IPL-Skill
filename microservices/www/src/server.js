38ddd66670e1bb9a70bb93a7014b794492dd996b
Saurabh Shubham <saurabh.friday@gmail.com>
2018-02-01 02:01:07 +0530

    Test modification 2

--- microservices/www/src/server.js
+++ microservices/www/src/server.js
@@ -1,32 +1,30 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var alexaVerifier = require('alexa-verifier');
+var verify = require('./verify');
+var forecaster = require('./forecaster');
 
 const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static('public'));
+
 app.use(bodyParser.json({
     verify: function getRawBody(req, res, buf) {
         req.rawBody = buf.toString();
     }
 }));
 
-function requestVerifier(req, res, next) {
-    alexaVerifier(
-        req.headers.signaturecertchainurl,
-        req.headers.signature,
-        req.rawBody,
-        function verificationCallback(err) {
-            if (err) {
-                res.status(401).json({ message: 'Verification Failure', error: err });
-            } else {
-                next();
-            }
-        }
-    );
-}
 
-app.post('/hasura-ipl', requestVerifier, function(req, res) {
+app.get('/', function(req, res) {
+    res.json({ message: 'The Hasura IPL is up and running.', since: (new Date()).toString() });
+});
+
+app.post('/hasura-ipl', verify, function(req, res) {
     // We'll fill this out later!
     res.json({ hello: 'world' });
 });
 
-app.listen(3000);
+app.listen(app.get('port'), function() {
+    console.log('The Hasura IPL is up and running on port %d', app.get('port'));
+});
