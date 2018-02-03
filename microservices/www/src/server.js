e961c270435a1d4a0690f324ea75f87056a7c533
Saurabh Shubham <saurabh.friday@gmail.com>
2018-02-03 19:09:52 +0530

    Added full hasura files

--- microservices/www/src/server.js
+++ microservices/www/src/server.js
@@ -20,21 +20,9 @@ app.get('/ipl', function(req, res) {
 
 alexaApp.express({
     expressApp: app,
-    //router: express.Router(),
-  
-    // verifies requests come from amazon alexa. Must be enabled for production.
-    // You can disable this if you're running a dev environment and want to POST
-    // things to test behavior. enabled by default.
     checkCert: false,
-  
-    // sets up a GET route when set to true. This is handy for testing in
-    // development, but not recommended for production. disabled by default
     debug: true
   });
-  
-  // now POST calls to /test in express will be handled by the app.request() function
-  
-  // from here on you can setup any other express routes or middlewares as normal
   
   alexaApp.launch(function(request, response) {
     response.say("You have launched the Hasura IPL skill.");
